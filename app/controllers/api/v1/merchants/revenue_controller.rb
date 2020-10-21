d057db2c84c65c8f977d04abf426d9c86d3e17ab
Hashim Gari <hashmaster3k@gmail.com>
2020-10-21 07:58:23 -0600

    Returns list of merchants who sold most items

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-     result = ActiveRecord::Base.connection.execute("SELECT m.id, SUM(ii.quantity * ii.unit_price) AS revenue FROM merchants m JOIN invoices v ON m.id = v.merchant_id JOIN invoice_items ii ON ii.invoice_id = v.id JOIN transactions t ON t.invoice_id = v.id WHERE t.result = 'success' AND v.status = 'shipped' GROUP BY m.id ORDER BY revenue DESC LIMIT #{params[:quantity]};")
+    return nil if params[:quantity].to_i < 1
 
-     merchants = result.pluck("id").map do |id|
-       Merchant.find(id)
-     end
+    if request.env["PATH_INFO"].split('/').last == "most_items"
+      result = ActiveRecord::Base.connection.execute("SELECT m.id, SUM(ii.quantity) AS sold FROM merchants m JOIN invoices v ON m.id = v.merchant_id JOIN invoice_items ii ON ii.invoice_id = v.id JOIN transactions t ON t.invoice_id = v.id WHERE t.result = 'success' AND v.status = 'shipped' GROUP BY m.id ORDER BY sold DESC LIMIT #{params[:quantity]};")
+    else #most_revenue
+      result = ActiveRecord::Base.connection.execute("SELECT m.id, SUM(ii.quantity * ii.unit_price) AS revenue FROM merchants m JOIN invoices v ON m.id = v.merchant_id JOIN invoice_items ii ON ii.invoice_id = v.id JOIN transactions t ON t.invoice_id = v.id WHERE t.result = 'success' AND v.status = 'shipped' GROUP BY m.id ORDER BY revenue DESC LIMIT #{params[:quantity]};")
+    end
 
-     render json: MerchantSerializer.new(merchants)
+    merchants = result.pluck("id").map do |id|
+      Merchant.find(id)
+    end
+
+    render json: MerchantSerializer.new(merchants)
   end
 end
--- spec/requests/api/v1/merchants/revenue_request_spec.rb
+++ spec/requests/api/v1/merchants/revenue_request_spec.rb
@@ -1,34 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe 'MERCHANT REVENUE API' do
-  describe 'happy' do
-    before :each do
-      cus = create(:customer)
-      mer1 = Merchant.create!(name: 'Billy')
-      it1 = mer1.items.create!(name: 'Frisbee', description: 'woo', unit_price: 3.50)
-      in1 = mer1.invoices.create!(customer_id: cus.id, status: 'shipped')
-      ii1 = it1.invoice_items.create!(invoice_id: in1.id, quantity: 2, unit_price: it1.unit_price)
-      it2 = mer1.items.create!(name: 'Ball', description: 'woo', unit_price: 2.75)
-      tr1 = in1.transactions.create!(credit_card_number: '123', credit_card_expiration_date: '', result: 'success')
-      mer2 = Merchant.create!(name: 'Bob')
-      it3 = mer2.items.create!(name: 'Bottle', description: 'woo', unit_price: 9.23)
-      it4 = mer2.items.create!(name: 'Container', description: 'woo', unit_price: 5.99)
-      @mer3 = Merchant.create!(name: 'Joel')
-      it5 = @mer3.items.create!(name: 'Sock', description: 'woo', unit_price: 5.55)
-      in2 = @mer3.invoices.create!(customer_id: cus.id, status: 'shipped')
-      ii2 = it5.invoice_items.create!(invoice_id: in2.id, quantity: 2, unit_price: it5.unit_price)
-      tr2 = in2.transactions.create!(credit_card_number: '123', credit_card_expiration_date: '', result: 'success')
-      it6 = @mer3.items.create!(name: 'Belt', description: 'woo', unit_price: 5.55)
+  describe 'merchants with most revenue' do
+    describe 'happy' do
+      before :each do
+        cus = create(:customer)
+        mer1 = Merchant.create!(name: 'Billy')
+        it1 = mer1.items.create!(name: 'Frisbee', description: 'woo', unit_price: 3.50)
+        in1 = mer1.invoices.create!(customer_id: cus.id, status: 'shipped')
+        ii1 = it1.invoice_items.create!(invoice_id: in1.id, quantity: 2, unit_price: it1.unit_price)
+        it2 = mer1.items.create!(name: 'Ball', description: 'woo', unit_price: 2.75)
+        tr1 = in1.transactions.create!(credit_card_number: '123', credit_card_expiration_date: '', result: 'success')
+        mer2 = Merchant.create!(name: 'Bob')
+        it3 = mer2.items.create!(name: 'Bottle', description: 'woo', unit_price: 9.23)
+        it4 = mer2.items.create!(name: 'Container', description: 'woo', unit_price: 5.99)
+        @mer3 = Merchant.create!(name: 'Joel')
+        it5 = @mer3.items.create!(name: 'Sock', description: 'woo', unit_price: 5.55)
+        in2 = @mer3.invoices.create!(customer_id: cus.id, status: 'shipped')
+        ii2 = it5.invoice_items.create!(invoice_id: in2.id, quantity: 2, unit_price: it5.unit_price)
+        tr2 = in2.transactions.create!(credit_card_number: '123', credit_card_expiration_date: '', result: 'success')
+        it6 = @mer3.items.create!(name: 'Belt', description: 'woo', unit_price: 5.55)
+      end
+
+      it 'returns a list of merchants with highest revenue' do
+        num_of_merchants = 3
+        get "/api/v1/merchants/most_revenue?quantity=#{num_of_merchants}"
+        expect(response).to be_successful
+        json = JSON(response.body, symbolize_names: true)
+        expect(json[:data]).to be_a(Array)
+        expect(json[:data].count).to eq(2)
+        expect(json[:data].first[:id].to_i).to eq(@mer3.id)
+      end
     end
 
-    it 'returns a list of merchants with highest revenue' do
-      num_of_merchants = 3
-      get "/api/v1/merchants/most_revenue?quantity=#{num_of_merchants}"
-      expect(response).to be_successful
-      json = JSON(response.body, symbolize_names: true)
-      expect(json[:data]).to be_a(Array)
-      expect(json[:data].count).to eq(2)
-      expect(json[:data].first[:id].to_i).to eq(@mer3.id)
+    describe 'sad' do
+      it 'returns a 204 if query entered wrong' do
+        get '/api/v1/merchants/most_revenue?name=blonde'
+        expect(response).to be_successful
+        expect(response.status).to eq(204)
+      end
+    end
+  end
+
+  describe 'merchants with most items sold' do
+    describe 'happy' do
+      before :each do
+        cus = create(:customer)
+        @mer1 = Merchant.create!(name: 'Billy')
+        it1 = @mer1.items.create!(name: 'Frisbee', description: 'woo', unit_price: 3.50)
+        in1 = @mer1.invoices.create!(customer_id: cus.id, status: 'shipped')
+        ii1 = it1.invoice_items.create!(invoice_id: in1.id, quantity: 5, unit_price: it1.unit_price)
+        it2 = @mer1.items.create!(name: 'Ball', description: 'woo', unit_price: 2.75)
+        tr1 = in1.transactions.create!(credit_card_number: '123', credit_card_expiration_date: '', result: 'success')
+        mer2 = Merchant.create!(name: 'Bob')
+        it3 = mer2.items.create!(name: 'Bottle', description: 'woo', unit_price: 9.23)
+        it4 = mer2.items.create!(name: 'Container', description: 'woo', unit_price: 5.99)
+        @mer3 = Merchant.create!(name: 'Joel')
+        it5 = @mer3.items.create!(name: 'Sock', description: 'woo', unit_price: 5.55)
+        in2 = @mer3.invoices.create!(customer_id: cus.id, status: 'shipped')
+        ii2 = it5.invoice_items.create!(invoice_id: in2.id, quantity: 2, unit_price: it5.unit_price)
+        tr2 = in2.transactions.create!(credit_card_number: '123', credit_card_expiration_date: '', result: 'success')
+        it6 = @mer3.items.create!(name: 'Belt', description: 'woo', unit_price: 5.55)
+      end
+
+      it 'returns list of ranked merchants with most items sold' do
+        num_of_merchants = 3
+        get "/api/v1/merchants/most_items?quantity=#{num_of_merchants}"
+        expect(response).to be_successful
+        json = JSON(response.body, symbolize_names: true)
+        expect(json[:data]).to be_a(Array)
+        expect(json[:data].count).to eq(2)
+        expect(json[:data].first[:id].to_i).to eq(@mer1.id)
+      end
+    end
+
+    describe 'sad' do
+      it 'returns a 204 if query entered wrong' do
+        get '/api/v1/merchants/most_revenue?name=3.2'
+        expect(response).to be_successful
+        expect(response.status).to eq(204)
+      end
     end
   end
 end
