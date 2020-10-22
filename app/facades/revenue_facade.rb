339eb6d75c3fd0bb2396015e4b4299463a27f90b
Hashim Gari <hashmaster3k@gmail.com>
2020-10-22 14:46:45 -0600

    SQL -> AR for merchant with most revenue

--- app/facades/revenue_facade.rb
+++ app/facades/revenue_facade.rb
@@ -27,6 +27,12 @@ class RevenueFacade
   end
 
   def self.merchant_with_most_revenue(quantity)
-    ActiveRecord::Base.connection.execute("SELECT m.id, SUM(ii.quantity * ii.unit_price) AS revenue FROM merchants m JOIN invoices v ON m.id = v.merchant_id JOIN invoice_items ii ON ii.invoice_id = v.id JOIN transactions t ON t.invoice_id = v.id WHERE t.result = 'success' AND v.status = 'shipped' GROUP BY m.id ORDER BY revenue DESC LIMIT #{quantity};")
+    Merchant
+      .select("merchants.id, SUM(invoice_items.quantity * invoice_items.unit_price)")
+      .joins(:invoice_items, :transactions)
+      .where("transactions.result = 'success' AND invoices.status = 'shipped'")
+      .group("merchants.id")
+      .order(Arel.sql("SUM(invoice_items.quantity * invoice_items.unit_price) DESC"))
+      .limit(quantity)
   end
 end
