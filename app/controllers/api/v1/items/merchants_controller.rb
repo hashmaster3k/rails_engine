58f18a92e8a37452b416a9e67a331ec7e4137619
Hashim Gari <hashmaster3k@gmail.com>
2020-10-22 11:51:08 -0600

    Add item instance method

--- app/controllers/api/v1/items/merchants_controller.rb
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Items::MerchantsController < ApplicationController
   def show
+    return nil if !Item.is_item?(params[:id])
     item = Item.find(params[:id])
     render json: MerchantSerializer.new(Merchant.find(item.merchant_id))
   end
